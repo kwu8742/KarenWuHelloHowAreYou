83dae710b9e4b338a439b8cbc450e382dc4221f7
kwu8742 <35508178+kwu8742@users.noreply.github.com>
2018-12-14 14:11:53 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("What is your name?);
+	String name = input.nextLine();
+	System.out.println("Hello " + name + ", how are you?");
+	System.out.println("How old are you " + name + "?");
+	int age = input.nextInt();
+	System.out.println("You are " + name + "and you are " + age + "years old.");
     }
 }
